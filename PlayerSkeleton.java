daeadc88a819ee86a566f0243df60c634ccde032
Jaivignesh Venugopal <smugs1995@gmail.com>
2018-03-19 02:34:02 +0800

    PlayerSkeleton.java: Implement height difference as heuristic function

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -3,10 +3,56 @@ public class PlayerSkeleton {
 
 	//implement this function to have a working system
 	public int pickMove(State s, int[][] legalMoves) {
-		
-		return 0;
+	    int move = 0;
+
+	    move = selectState(s, legalMoves);
+
+	    return move;
 	}
-	
+
+	public int selectState(State state, int[][] legalMoves) {
+
+	    int minValue = 100;
+	    int bestState = 0;
+
+	    for (int stateNo = 0; stateNo < legalMoves.length; stateNo++) {
+
+	        try {
+                State simulateState = (State)state.clone();
+
+                int heuristicValue = findHeuristic(simulateState, stateNo, legalMoves);
+
+                if (heuristicValue < minValue) {
+                    minValue = heuristicValue;
+                    bestState = stateNo;
+                }
+            } catch (CloneNotSupportedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return bestState;
+    }
+
+    public int findHeuristic(State simulateState, int stateNo, int[][] legalMoves) {
+
+	    simulateState.makeMove(legalMoves[stateNo]);
+        int heuristicValue = calculateAbsHeightDiff(simulateState);
+        return heuristicValue;
+    }
+
+    public int calculateAbsHeightDiff(State simulateState) {
+
+	    int sum = 0;
+	    int top[] = simulateState.getTop();
+	    for(int i = 0; i < 9; i++) {
+            sum += Math.abs(top[i]-top[i+1]);
+	    }
+
+	    return sum;
+    }
+
+
 	public static void main(String[] args) {
 		State s = new State();
 		new TFrame(s);
